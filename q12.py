e42619d21907ebb675fb067ebd6e41c95c9e4b90
stacksmith17 <pandeyarchit1706@gmail.com>
2026-01-26 16:01:14 +0530

    new

---
+++ q12.py
@@ -0,0 +1,11 @@
+# check wether the word ids theb palindrome 
+s = input("Enter a string: ")
+rev = ""
+
+for ch in s:
+    rev = ch + rev
+
+if s == rev:
+    print("Palindrome")
+else:
+    print("Not a palindrome")
