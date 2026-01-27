f61f289771cf1714ec502e18333e4f1da19690b1
stacksmith17 <pandeyarchit1706@gmail.com>
2026-01-27 14:21:48 +0530

    separating positive and negative element in list

---
+++ lq1.py
@@ -0,0 +1,17 @@
+# this will contain the questions related to the list 
+
+# Print positive and negative elements of an List
+
+# lets take the example of the  l = [-90,20,-78,88,-69,-96,55] 
+
+l = [-90,20,-78,88,-69,-96,55] 
+
+print ("the positive element in the list are ")
+for i in l:
+    if i>=0:
+        print (i)
+
+print ("the negative elements in the list are ")
+for i in l :
+    if i < 0 :
+        print (i)
