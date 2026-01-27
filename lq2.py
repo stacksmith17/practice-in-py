4f916664a0d200eb4c6837dd877feb2abaeeeade
stacksmith17 <pandeyarchit1706@gmail.com>
2026-01-27 14:28:22 +0530

    mean of the element in list

---
+++ lq2.py
@@ -0,0 +1,12 @@
+# Mean of List elements 
+
+# lets take the example of the list l = [10,20,30,40,50,60,70,80,90]
+
+l = [10,20,30,40,50,60,70,80,90]
+
+sum = 0 
+
+for i in l :
+    sum = sum + i 
+
+print (sum/len(l))
