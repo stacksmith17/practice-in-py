76fd864c7aa2b4f0b49fe3ba1db5417cd7618951
stacksmith17 <pandeyarchit1706@gmail.com>
2026-01-28 20:39:23 +0530

    sum of elements in the dictionary

---
+++ dq2.py
@@ -0,0 +1,12 @@
+# print the sum of all  the element present in the dictionary 
+
+#let 
+d1 = {10:100,20:200,30:300,40:400,50:500,60:600}
+
+sum = 0 
+
+for i in d1:
+    sum = sum + d1[i]
+
+print (sum )
+
